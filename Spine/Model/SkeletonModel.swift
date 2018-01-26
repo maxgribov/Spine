31569f770c1c95e82285beb0bee73e32909d56a7
Max Gribov <gribov.max@gmail.com>
2018-01-27 00:34:19 +0300

    skeleton model basic documentation

--- Spine/Model/SkeletonModel.swift
+++ Spine/Model/SkeletonModel.swift
@@ -8,14 +8,39 @@
 
 import Foundation
 
+/// The skeleton section stores metadata about the skeleton.
 struct SkeletonModel {
 
+    /// A hash of all the skeleton data. This can be used by tools to detect if the data has changed since the last time it was loaded.
     let hash: String
+    /// The version of Spine that exported the data. This can be used by tools to enforce a particular Spine version to be used.
     let spine: String
+    /**
+     The AABB size for the skeleton's attachments as it was in the setup pose in Spine
+     
+     - width: The AABB width for the skeleton's attachments as it was in the setup pose in Spine. This can be used as a general size of the skeleton, though the skeleton's AABB depends on how it is posed.
+     - height: The AABB height for the skeleton's attachments as it was in the setup pose in Spine.
+     */
     let size: CGSize
+    /// The dopesheet framerate in frames per second, as it was in Spine. Assume 30 if omitted. Nonessential.
     let fps: CGFloat
+    
+    ///The images path, as it was in Spine. Nonessential.
     let path: String?
 
+    /**
+     Initializes a new skeleton.
+     
+     - Parameters:
+        - hash: Required
+        - spine: Required
+        - width: Required
+        - height: Required
+        - fps: Optional, default: 30.0
+        - path: Optional
+     
+     - Returns: new skeleton.
+     */
     init(_ hash: String, _ spine: String, _ width: CGFloat, _ height: CGFloat, _ fps: CGFloat?, _ path: String?) {
         
         self.hash = hash
@@ -54,19 +79,65 @@ extension SkeletonModel: Decodable {
 
 //MARK: - Bone
 
+/// The bones section describes the bones in the setup pose.
 struct BoneModel {
     
+    /// The bone name. This is unique for the skeleton.
     let name: String
+    /// Parent bone. Nil if omitted.
     let parent: String?
+    /// The length of the bone. The bone length is not typically used at runtime except to draw debug lines for the bones. Assume 0 if omitted.
     let lenght: CGFloat
+    /// Determines how parent bone transforms are inherited: normal, onlyTranslation, noRotationOrReflection, noScale, or noScaleOrReflection. Assume normal if omitted.
     let transform: BoneTransformModelType
+    /**
+     The position of the bone relative to the parent for the setup pose.
+     
+     - x: The X position of the bone relative to the parent for the setup pose. Assume 0 if omitted.
+     - y: The Y position of the bone relative to the parent for the setup pose. Assume 0 if omitted.
+     */
     let position: CGPoint
+    /// The rotation in degrees of the bone relative to the parent for the setup pose. Assume 0 if omitted.
     let rotation: CGFloat
+    /**
+     The scale of the bone for the setup pose.
+     
+     - dx: The X scale of the bone for the setup pose. Assume 1 if omitted.
+     - dy: The Y scale of the bone for the setup pose. Assume 1 if omitted.
+     */
     let scale: CGVector
+    /**
+     The shear of the bone for the setup pose.
+     
+     - dx: The X shear of the bone for the setup pose. Assume 0 if omitted.
+     - dy: The Y shear of the bone for the setup pose. Assume 0 if omitted.
+     */
     let shear: CGVector
+    /// False if scale from parent bones should not affect this bone. Assume true if omitted.
     let inheritScale: Bool
+    /// False if rotation from parent bones should not affect this bone. Assume true if omitted.
     let inheritRotation: Bool
     
+    /**
+     Initializes a new bone.
+     
+     - Parameters:
+         - name: Required
+         - parent: Optional
+         - lenght: Optional, default: 0
+         - transform: Optional, default: .normal
+         - x: Optional, default: 0
+         - y: Optional, default: 0
+         - rotation: Optional, default: 0
+         - scaleX: Optional, default: 1.0
+         - scaleY: Optional, default: 1.0
+         - shearX: Optional, default: 0
+         - shearY: Optional, default: 0
+         - inheritScale: Optional, default: true
+         - inheritRotation: Optional, default: true
+
+     - Returns: new bone.
+     */
     init(_ name: String, _ parent: String?, _ lenght: CGFloat?, _ transform: Int?, _ x: CGFloat?, _ y: CGFloat?, _ rotation: CGFloat?, _ scaleX: CGFloat?, _ scaleY: CGFloat?, _ shearX: CGFloat?, _ shearY: CGFloat?, _ inheritScale: Bool?, _ inheritRotation: Bool?) {
         
         self.name = name
@@ -123,15 +194,35 @@ extension BoneModel: Decodable {
 
 //MARK: - Slot
 
+/// The slots section describes the draw order and the available slots where attachments can be assigned.
 struct SlotModel {
     
+    /// The slot name. This is unique for the skeleton.
     let name: String
+    /// The name of the bone that this slot is attached to.
     let bone: String
+    /// The color of the slot for the setup pose. This is an 8 character string containing 4 two digit hex numbers in RGBA order. Assume "FF" for alpha if alpha is omitted. Assume "FFFFFFFF" if omitted.
     let color: ColorModel
+    /// The dark color of the slot for the setup pose, used for two color tinting. This is a 6 character string containing 3 two digit hex numbers in RGB order. Omitted when two color tinting is not used.
     let dark: ColorModel?
+    /// The name of the slot's attachment for the setup pose. Assume no attachment for the setup pose if omitted.
     let attachment: String?
+    /// The type of blending to use when drawing the slot's visible attachment: normal, additive, multiply, or screen.
     let blend: BlendModeModelType
     
+    /**
+     Initializes a new slot.
+     
+     - Parameters:
+         - name: Required
+         - bone: Required
+         - color: Optional, default: FFFFFFFF
+         - dark: Optional
+         - attachment: Optional
+         - blend: Optional, default: .normal
+     
+     - Returns: new slot.
+     */
     init(_ name: String, _ bone: String, _ color: String?, _ dark: String?, _ attachment: String?, _ blend: Int?) {
         
         self.name = name
