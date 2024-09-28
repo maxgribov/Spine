3411b998dc5ded9e258ffefd1bba466880242f61
Max Gribov <gribov.max@gmail.com>
2024-09-29 01:42:52 +0300

    Fix package platforms versions to support `Identifiable` protocol (#66)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Spine",
     platforms: [
-        .iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v3)
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         .library(name: "Spine", targets: ["Spine"]),
